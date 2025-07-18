8af5179ea9888f3d300cbc32d07d5ad5004de0cf
Tim Culverhouse <tim@timculverhouse.com>
2025-07-18 13:38:38 -0500

    printer: add avahi, disable resolved multicast

--- install/80-printer.fish
+++ install/80-printer.fish
@@ -1,4 +1,5 @@
 yay -S --noconfirm --needed \
+    avahi \
     cups \
     cups-filters \
     cups-pdf \
@@ -8,6 +9,12 @@ yay -S --noconfirm --needed \
     foomatic-db-nonfree \
     foomatic-db-nonfree-ppds \
     foomatic-db-ppds \
+    nss-mdns \
     system-config-printer
 
 sudo systemctl enable --now cups.service
+
+sudo mkdir -p /etc/systemd/resolved.conf.d
+echo "[Resolve]\nMulticastDNS=no" | sudo tee /etc/systemd/resolved.conf.d/10-disable-multicast.conf
+
+sudo systemctl enable --now avahi-daemon.service
