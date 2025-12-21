3fd15344b04e42664bcbf3305f6bc2653963bfdb
Tim Culverhouse <tim@timculverhouse.com>
2025-12-21 15:26:42 -0600

    Enable mDNS hostname resolution for printer discovery

--- install/80-printer.fish
+++ install/80-printer.fish
@@ -18,3 +18,6 @@ sudo mkdir -p /etc/systemd/resolved.conf.d
 echo "[Resolve]\nMulticastDNS=no" | sudo tee /etc/systemd/resolved.conf.d/10-disable-multicast.conf
 
 sudo systemctl enable --now avahi-daemon.service
+
+# Enable mDNS hostname resolution for .local addresses
+sudo sed -i 's/^hosts:.*/hosts: mymachines mdns_minimal [NOTFOUND=return] resolve [!UNAVAIL=return] files myhostname dns/' /etc/nsswitch.conf
