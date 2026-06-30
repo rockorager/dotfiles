f4fc96dd0a4142255826852a85fc4813414034df
Tim Culverhouse <tim@timculverhouse.com>
2026-06-30 13:53:17 -0500

    Remove unused virtual mic setup

--- install/16-audio.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-yay -S --noconfirm --needed obs-studio easyeffects lsp-plugins
-
-mkdir -p "$HOME/.config/systemd/user"
-ln -sf "$HOME/.local/share/dotfiles/services/virtual-mic.service" "$HOME/.config/systemd/user/"
-systemctl --user daemon-reload
-systemctl enable --now --user virtual-mic.service
