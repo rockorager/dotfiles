09f28433852fdeef0193be45157662edbe293246
Tim Culverhouse <tim@timculverhouse.com>
2026-06-30 14:18:44 -0500

    fonts: use Fira Code monospace
    Set Fira Code as the Ghostty font and the system monospace preference through fontconfig.
    
    Install ttf-fira-code so fresh machines resolve the same monospace font.

--- install/13-fonts.sh
+++ install/13-fonts.sh
@@ -4,6 +4,7 @@ yay -S --noconfirm --needed \
     adwaita-fonts \
     noto-fonts \
     noto-fonts-emoji \
+    ttf-fira-code \
     ttf-font-awesome \
     ttf-nerd-fonts-symbols \
     ttf-victor-mono
