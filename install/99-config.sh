0a122f5c3befa6b68bba64815b2ab5630cc44b27
Tim Culverhouse <tim@timculverhouse.com>
2026-06-30 14:08:09 -0500

    Symlink managed config files

--- install/99-config.sh
+++ install/99-config.sh
@@ -1,18 +1,44 @@
 #!/bin/sh
 
+dotfiles_dir=${DOTFILES_DIR:-"$HOME/.local/share/dotfiles"}
+
+link_config() {
+    src=$1
+    dest=$2
+
+    if [ -e "$dest" ] || [ -L "$dest" ]; then
+        rm -rf "$dest"
+    fi
+
+    mkdir -p "$(dirname "$dest")"
+    ln -s "$src" "$dest"
+}
+
 mkdir -p "$HOME/.config"
 
-cp --recursive --verbose "$HOME/.local/share/dotfiles"/config/* "$HOME/.config/"
+for config in \
+    environment.d \
+    fontconfig \
+    foot \
+    fuzzel \
+    ghostty \
+    kanshi \
+    mako \
+    nvim \
+    sway \
+    swaylock \
+    waybar
+do
+    link_config "$dotfiles_dir/config/$config" "$HOME/.config/$config"
+done
 
 mkdir -p "$HOME/.config/fish"
+link_config "$dotfiles_dir/config/fish/config.fish" "$HOME/.config/fish/config.fish"
+link_config "$dotfiles_dir/config/fish/conf.d" "$HOME/.config/fish/conf.d"
+link_config "$dotfiles_dir/config/fish/functions" "$HOME/.config/fish/functions"
+rm -f "$HOME/.config/fish/default.fish"
 
-if ! test -f "$HOME/.config/fish/config.fish"; then
-    printf '%s\n' \
-        '# Managed defaults live in default.fish; put local/private config below.' \
-        'source $__fish_config_dir/default.fish' \
-        '' \
-        > "$HOME/.config/fish/config.fish"
-fi
-
-touch "$HOME/.config/git/config"
-git config --global include.path default
+mkdir -p "$HOME/.config/git"
+link_config "$dotfiles_dir/config/git/config" "$HOME/.config/git/config"
+link_config "$dotfiles_dir/config/git/allowed_signers" "$HOME/.config/git/allowed_signers"
+rm -f "$HOME/.config/git/default"
