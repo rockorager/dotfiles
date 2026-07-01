535732b15b3817553e578b90e46f03eb6a766643
Tim Culverhouse <tim@timculverhouse.com>
2026-07-01 10:31:41 -0500

    linux: tune power profile on external power
    Switch the laptop to the performance profile when external power is available and back to balanced when running on battery. The helper also forces the CPU governor so the kernel state matches the desired plugged-in performance policy.
    
    Install a systemd oneshot and udev rule so power-supply changes reapply the policy automatically. Enable thermald as part of the same power setup and add the compatibility override needed for this platform to keep the daemon running.

--- install/linux/17-power.sh
+++ install/linux/17-power.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
 yay -S --noconfirm --needed \
-    power-profiles-daemon
+    power-profiles-daemon \
+    thermald
 
+sudo install -Dm755 ~/.local/share/dotfiles/system-bin/power-profile-auto /usr/local/bin/power-profile-auto
+sudo install -Dm644 ~/.local/share/dotfiles/system-services/power-profile-auto.service /etc/systemd/system/power-profile-auto.service
+sudo install -Dm644 ~/.local/share/dotfiles/system-services/thermald.service.d/override.conf /etc/systemd/system/thermald.service.d/override.conf
+sudo install -Dm644 ~/.local/share/dotfiles/system-udev-rules/90-power-profile-auto.rules /etc/udev/rules.d/90-power-profile-auto.rules
+
+sudo systemctl daemon-reload
 sudo systemctl enable --now power-profiles-daemon.service
+sudo systemctl enable --now thermald.service
+sudo systemctl start power-profile-auto.service
 
 sudo install -Dm644 ~/.local/share/dotfiles/system-udev-rules/90-tobenone-dock-usb-power.rules /etc/udev/rules.d/90-tobenone-dock-usb-power.rules
 sudo udevadm control --reload-rules
+sudo udevadm trigger --subsystem-match=power_supply
 sudo udevadm trigger --subsystem-match=usb
